0c9a39f1b8425074bd3ee7e3e8ce32323f792cb0
NandiniUpadhyay <nandiniupadhyay1902@gmail.com>
2021-03-25 10:05:57 +0530

    added first maven everything

---
+++ NandiniUpadhyay-creator/first-git-repo.git/first-maven-app/src/main/java/com/lti/model/Employee.java
@@ -0,0 +1,27 @@
+package com.lti.model;
+
+public class Employee {
+	
+	int empId;
+	String empName;
+	public int getEmpId(){
+		return empId;
+		
+	}
+
+	public void setEmpId(int empId) {
+			this.empId=empId;
+	}
+
+	public void setEmpId(String empName) {
+			this.empName=empName;
+	}
+	
+	public String getEmpName(){
+		return empName;
+		
+	}
+
+
+	
+}
